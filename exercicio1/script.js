392a60e4a934abb4758c52814f5d9959cb8b845a
Davi <Daviandreozzi7@gmail.com>
2022-07-25 22:20:07 -0300

    a009-strings

---
+++ exercicio1/script.js
@@ -0,0 +1,4 @@
+console.log(prompt`'Qual o seu nome? ' ` 
++ prompt`'Qual sua comida Favorita?' ` 
++ prompt`'Qual sua Doce Favorito?'` 
++ prompt`'Qual seu lanche Favorito?' `)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,9 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaStrings = minhaString.trim()
+console.log(minhaString.trim())
+
+const tamanho = minhaString.length
+console.log('A frase tem', tamanho, 'Caracteres!')
+
+const mudar = minhaString.replaceAll('________', '“sticioso”')
+console.log(mudar)
